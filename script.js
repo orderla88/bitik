e498cd2d391021be31eca28ab09a35d1a3a5bd86
orderla88 <88731497+orderla88@users.noreply.github.com>
2026-04-07 09:45:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js', { scope: './' })
+  navigator.serviceWorker.register('/BITIK/sw.js', { scope: '/BITIK/' })
     .then((reg) => console.log('Service worker registered:', reg))
     .catch((err) => console.error('Service worker not registered:', err));
 }
