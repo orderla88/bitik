32850cd56229a26b78377231e665ac6153c34e7f
orderla88 <88731497+orderla88@users.noreply.github.com>
2026-04-13 13:33:27 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Version your cache to force updates when you change files
-const CACHE_NAME = 'static-cache-v3';
+const CACHE_NAME = 'static-cache-v4';
 
 // Precache explicit files (from your list)
 const PRECACHE_URLS = [
